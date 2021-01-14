f4d040ba770fa1940999f3edbce9475db15ed799
Masyanov <61470928+Masyanov@users.noreply.github.com>
2021-01-14 18:26:35 +0300

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -1,25 +1,11 @@
 $( function() {
-  $( "#accordion" ).accordion( {
+  $( "#accordion" ).accordion({
     collapsible: true
-  } );
+  });
 } );
 
-$(document).ready(function() {
-
-  $(".faq__item").click(function() {
-if($(this).next("div").is(":visible")){
-$(this).next("div").slideUp("slow");
-} else {
-$(".faq__content").slideUp("slow");
-$(this).next("div").slideToggle("slow");
-}
-
-
-
-});
-
-$( "span" ).click(function() {
-  $( this ).toggleClass( "plus-active" );
-});
-});
-
+$( function() {
+  $( "#accordion" ).on( "click", function() {
+    $( ".plus" ).toggleClass( "plus-active", 1000 );
+  });
+} );
