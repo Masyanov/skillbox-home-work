84720b3273ad5e0d4353b35ab5468e6245ebbeb5
masyanov.aleksei@gmail.com <masyanov.aleksei@gmail.com>
2021-01-19 14:54:31 +0300

    change

--- js/accordion.js
+++ js/accordion.js
@@ -4,11 +4,3 @@ $( function() {
   });
 } );
 
-<<<<<<< HEAD
-=======
-$( function() {
-  $( "#accordion" ).on( "click", function() {
-    $( ".plus" ).toggleClass( "plus-active", 1000 );
-  });
-} );
->>>>>>> 1a68aed8405d8063a2f72b6c751fe105f5db1700
