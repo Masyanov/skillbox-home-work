f993092b9098a22583f26e2325e688f3b0b5e3ac
masyanov.aleksei@gmail.com <masyanov.aleksei@gmail.com>
2021-01-19 14:52:13 +0300

    change

--- js/accordion.js
+++ js/accordion.js
@@ -1,25 +1,6 @@
 $( function() {
-  $( "#accordion" ).accordion( {
+  $( "#accordion" ).accordion({
     collapsible: true
-  } );
+  });
 } );
-
-$(document).ready(function() {
-
-  $(".faq__item").click(function() {
-if($(this).next("div").is(":visible")){
-$(this).next("div").slideUp("slow");
-} else {
-$(".faq__content").slideUp("slow");
-$(this).next("div").slideToggle("slow");
-}
-
-
-
-});
-
-$( "span" ).click(function() {
-  $( this ).toggleClass( "plus-active" );
-});
-});
 
